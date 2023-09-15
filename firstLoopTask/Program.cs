14ead1347e2e5c68e590821f424bd1a9b68f78a8
e.georgieva24 <e.georgieva24@LAB108PC13>
2023-09-15 12:12:14 +0300

    Add project files.

---
+++ firstLoopTask/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace firstLoopTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0; 
+            for (int i = 1;i < 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    count++;
+                    Console.Write(count + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
